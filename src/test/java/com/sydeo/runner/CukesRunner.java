4f9dd3af4c1af8f509112ab2ba2371d097e68c1f
Vick Wladowski <vvladovskiy@gmail.com>
2022-06-29 11:21:09 -0700

    Initial commit

---
+++ src/test/java/com/sydeo/runner/CukesRunner.java
@@ -0,0 +1,16 @@
+package com.sydeo.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/cucumber-reports.html",
+        features = "src/test/resources/features" ,
+        glue = "com/cydeo/step_definitions" ,
+        dryRun = false,
+        tags ="@eating"
+)
+public class CukesRunner {
+}
