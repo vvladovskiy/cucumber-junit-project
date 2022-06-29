058ca980322feaa4b589e8cd0987e54626c51fd8
Vick Wladowski <vvladovskiy@gmail.com>
2022-06-29 11:26:14 -0700

    My cucumber Project

--- src/test/java/com/sydeo/utilities/ConfigurationReader.java
+++ src/test/java/com/sydeo/utilities/ConfigurationReader.java
@@ -1,4 +1,4 @@
-package com.sydeo.utiilities;
+package com.sydeo.utilities;
 
 import java.io.FileInputStream;
 import java.io.IOException;
--- src/test/java/com/sydeo/utilities/Driver.java
+++ src/test/java/com/sydeo/utilities/Driver.java
@@ -1,4 +1,4 @@
-package com.sydeo.utiilities;
+package com.sydeo.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
