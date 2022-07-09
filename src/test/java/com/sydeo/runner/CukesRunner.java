02f0b2ef569caa9627f69af7d167161be02952c9
Vick <vvladovskiy@gmail.com>
2022-07-09 16:40:56 -0700

    Add a lot

--- src/test/java/com/sydeo/runner/CukesRunner.java
+++ src/test/java/com/sydeo/runner/CukesRunner.java
@@ -6,11 +6,15 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-reports.html",
+        plugin = {
+        "html:target/cucumber-report.html",
+        "rerun:target/rerun.txt" ,
+        "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+},
         features = "src/test/resources/features" ,
         glue = "com/cydeo/step_definitions" ,
         dryRun = false,
-        tags ="@driven1"
+        tags ="@driven1 and @driven2"
 )
 public class CukesRunner {
 }
